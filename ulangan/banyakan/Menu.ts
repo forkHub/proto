55154829ce426e3372e569b852d1c75d6ac94513
fajar <rokhman.fajar@gmail.com>
2019-12-08 10:26:08 +0700

    a

--- ulangan/banyakan/Menu.ts
+++ ulangan/banyakan/Menu.ts
@@ -5,29 +5,34 @@ import { Game } from "./Game.js";
 export class Menu extends BaseComponent {
 	private puluhanTbl: TombolMenu = null;
 	private banyakanTbl: TombolMenu = null;
+	private cont: HTMLDivElement = null;
 
 	constructor() {
 		super();
 		this._template = `
 			<div class='menu'>
-
+				<p>Belajar Matematika Tingkat Dasar</p>
+				<div class='cont'>
+				</div>
 			</div>
 		`;
+
+		this.build();
+		this.cont = this.getEl('div.cont') as HTMLDivElement;
 
 		this.puluhanTbl = new TombolMenu();
 		this.banyakanTbl = new TombolMenu();
 	}
 
 	init(): void {
-		this.puluhanTbl.label = 'Puluhan Satuan';
+		this.puluhanTbl.label = 'Puluhan dan Satuan';
 		this.puluhanTbl.onClick = () => {
 			this.puluhanClick();
 		}
-		this.puluhanTbl.attach(this._elHtml);
+		this.puluhanTbl.attach(this.cont);
 
 		this.banyakanTbl.label = 'Membandingkan';
-		this.banyakanTbl.attach(this._elHtml);
-		this.banyakanTbl.attach(this._elHtml)
+		this.banyakanTbl.attach(this.cont);
 	}
 
 	puluhanClick(): void {
--- ulangan/banyakan/js/Menu.js
+++ ulangan/banyakan/js/Menu.js
@@ -6,23 +6,27 @@ export class Menu extends BaseComponent {
         super();
         this.puluhanTbl = null;
         this.banyakanTbl = null;
+        this.cont = null;
         this._template = `
 			<div class='menu'>
-
+				<p>Belajar Matematika Tingkat Dasar</p>
+				<div class='cont'>
+				</div>
 			</div>
 		`;
+        this.build();
+        this.cont = this.getEl('div.cont');
         this.puluhanTbl = new TombolMenu();
         this.banyakanTbl = new TombolMenu();
     }
     init() {
-        this.puluhanTbl.label = 'Puluhan Satuan';
+        this.puluhanTbl.label = 'Puluhan dan Satuan';
         this.puluhanTbl.onClick = () => {
             this.puluhanClick();
         };
-        this.puluhanTbl.attach(this._elHtml);
+        this.puluhanTbl.attach(this.cont);
         this.banyakanTbl.label = 'Membandingkan';
-        this.banyakanTbl.attach(this._elHtml);
-        this.banyakanTbl.attach(this._elHtml);
+        this.banyakanTbl.attach(this.cont);
     }
     puluhanClick() {
         this.detach();
