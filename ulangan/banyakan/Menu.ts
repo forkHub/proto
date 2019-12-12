e3652ef4b7327b9b177fa1320d13a7b26044e03d
fajar <rokhman.fajar@gmail.com>
2019-12-13 06:12:36 +0700

    a

--- ulangan/banyakan/Menu.ts
+++ ulangan/banyakan/Menu.ts
@@ -5,6 +5,9 @@ import { Game } from "./Game.js";
 export class Menu extends BaseComponent {
 	private puluhanTbl: TombolMenu = null;
 	private banyakanTbl: TombolMenu = null;
+	private banyakanGambar3Tbl: TombolMenu = null;
+	private banyakanAngka2Tbl: TombolMenu = null;
+	private banyakanAngka3Tbl: TombolMenu = null;
 	private cont: HTMLDivElement = null;
 
 	constructor() {
@@ -22,20 +25,69 @@ export class Menu extends BaseComponent {
 
 		this.puluhanTbl = new TombolMenu();
 		this.banyakanTbl = new TombolMenu();
+		this.banyakanGambar3Tbl = new TombolMenu();
+		this.banyakanAngka2Tbl = new TombolMenu();
+		this.banyakanAngka3Tbl = new TombolMenu();
 	}
 
 	init(): void {
-		this.puluhanTbl.label = 'Puluhan dan Satuan';
-		this.puluhanTbl.onClick = () => {
-			this.puluhanClick();
-		}
-		this.puluhanTbl.attach(this.cont);
-
-		this.banyakanTbl.label = 'Membandingkan';
+		this.banyakanTbl.label = 'Membandingkan <br/> dengan gambar';
 		this.banyakanTbl.attach(this.cont);
 		this.banyakanTbl.onClick = () => {
 			this.banyakanClick();
 		}
+
+		this.banyakanGambar3Tbl.label = 'Membandingkan <br/> dengan gambar (3 angka)';
+		this.banyakanGambar3Tbl.attach(this.cont);
+		this.banyakanGambar3Tbl.onClick = () => {
+			this.banyakanGambar3Click();
+		}
+
+		this.banyakanAngka2Tbl.label = 'Membandingkan <br/> tanpa gambar';
+		this.banyakanAngka2Tbl.attach(this.cont);
+		this.banyakanAngka2Tbl.onClick = () => {
+			this.banyakanAngka2Click();
+		}
+
+		this.banyakanAngka3Tbl.label = 'Membandingkan <br/> tanpa gambar (3 angka)';
+		this.banyakanAngka3Tbl.attach(this.cont);
+		this.banyakanAngka3Tbl.onClick = () => {
+			this.banyakanAngka3Click();
+		}
+
+		this.puluhanTbl.label = 'Puluhan dan Satuan';
+		this.puluhanTbl.attach(this.cont);
+		this.puluhanTbl.onClick = () => {
+			this.puluhanClick();
+		}
+
+	}
+
+	banyakanAngka2Click(): void {
+		console.log('banyakan click gambar 3');
+		this.detach();
+		Game.inst.banyakan.jmlSoal = 2;
+		Game.inst.banyakan.angkaSaja = true;
+		Game.inst.banyakan.attach(Game.inst.cont);
+		Game.inst.banyakan.mulaiLagi();
+	}
+
+	banyakanAngka3Click(): void {
+		console.log('banyakan click gambar 3');
+		this.detach();
+		Game.inst.banyakan.jmlSoal = 3;
+		Game.inst.banyakan.angkaSaja = true;
+		Game.inst.banyakan.attach(Game.inst.cont);
+		Game.inst.banyakan.mulaiLagi();
+	}
+
+	banyakanGambar3Click(): void {
+		console.log('banyakan click gambar 3');
+		this.detach();
+		Game.inst.banyakan.jmlSoal = 3;
+		Game.inst.banyakan.angkaSaja = false;
+		Game.inst.banyakan.attach(Game.inst.cont);
+		Game.inst.banyakan.mulaiLagi();
 	}
 
 	puluhanClick(): void {
@@ -47,8 +99,8 @@ export class Menu extends BaseComponent {
 	banyakanClick(): void {
 		console.log('banyakan click');
 		this.detach();
-		Game.inst.banyakan.jmlSoal = 3;
-		Game.inst.banyakan.angkaSaja = true;
+		Game.inst.banyakan.jmlSoal = 2;
+		Game.inst.banyakan.angkaSaja = false;
 		Game.inst.banyakan.attach(Game.inst.cont);
 		Game.inst.banyakan.mulaiLagi();
 	}
--- ulangan/banyakan/js/Menu.js
+++ ulangan/banyakan/js/Menu.js
@@ -6,6 +6,9 @@ export class Menu extends BaseComponent {
         super();
         this.puluhanTbl = null;
         this.banyakanTbl = null;
+        this.banyakanGambar3Tbl = null;
+        this.banyakanAngka2Tbl = null;
+        this.banyakanAngka3Tbl = null;
         this.cont = null;
         this._template = `
 			<div class='menu'>
@@ -18,18 +21,60 @@ export class Menu extends BaseComponent {
         this.cont = this.getEl('div.cont');
         this.puluhanTbl = new TombolMenu();
         this.banyakanTbl = new TombolMenu();
+        this.banyakanGambar3Tbl = new TombolMenu();
+        this.banyakanAngka2Tbl = new TombolMenu();
+        this.banyakanAngka3Tbl = new TombolMenu();
     }
     init() {
-        this.puluhanTbl.label = 'Puluhan dan Satuan';
-        this.puluhanTbl.onClick = () => {
-            this.puluhanClick();
-        };
-        this.puluhanTbl.attach(this.cont);
-        this.banyakanTbl.label = 'Membandingkan';
+        this.banyakanTbl.label = 'Membandingkan <br/> dengan gambar';
         this.banyakanTbl.attach(this.cont);
         this.banyakanTbl.onClick = () => {
             this.banyakanClick();
         };
+        this.banyakanGambar3Tbl.label = 'Membandingkan <br/> dengan gambar (3 angka)';
+        this.banyakanGambar3Tbl.attach(this.cont);
+        this.banyakanGambar3Tbl.onClick = () => {
+            this.banyakanGambar3Click();
+        };
+        this.banyakanAngka2Tbl.label = 'Membandingkan <br/> tanpa gambar';
+        this.banyakanAngka2Tbl.attach(this.cont);
+        this.banyakanAngka2Tbl.onClick = () => {
+            this.banyakanAngka2Click();
+        };
+        this.banyakanAngka3Tbl.label = 'Membandingkan <br/> tanpa gambar (3 angka)';
+        this.banyakanAngka3Tbl.attach(this.cont);
+        this.banyakanAngka3Tbl.onClick = () => {
+            this.banyakanAngka3Click();
+        };
+        this.puluhanTbl.label = 'Puluhan dan Satuan';
+        this.puluhanTbl.attach(this.cont);
+        this.puluhanTbl.onClick = () => {
+            this.puluhanClick();
+        };
+    }
+    banyakanAngka2Click() {
+        console.log('banyakan click gambar 3');
+        this.detach();
+        Game.inst.banyakan.jmlSoal = 2;
+        Game.inst.banyakan.angkaSaja = true;
+        Game.inst.banyakan.attach(Game.inst.cont);
+        Game.inst.banyakan.mulaiLagi();
+    }
+    banyakanAngka3Click() {
+        console.log('banyakan click gambar 3');
+        this.detach();
+        Game.inst.banyakan.jmlSoal = 3;
+        Game.inst.banyakan.angkaSaja = true;
+        Game.inst.banyakan.attach(Game.inst.cont);
+        Game.inst.banyakan.mulaiLagi();
+    }
+    banyakanGambar3Click() {
+        console.log('banyakan click gambar 3');
+        this.detach();
+        Game.inst.banyakan.jmlSoal = 3;
+        Game.inst.banyakan.angkaSaja = false;
+        Game.inst.banyakan.attach(Game.inst.cont);
+        Game.inst.banyakan.mulaiLagi();
     }
     puluhanClick() {
         this.detach();
@@ -39,8 +84,8 @@ export class Menu extends BaseComponent {
     banyakanClick() {
         console.log('banyakan click');
         this.detach();
-        Game.inst.banyakan.jmlSoal = 3;
-        Game.inst.banyakan.angkaSaja = true;
+        Game.inst.banyakan.jmlSoal = 2;
+        Game.inst.banyakan.angkaSaja = false;
         Game.inst.banyakan.attach(Game.inst.cont);
         Game.inst.banyakan.mulaiLagi();
     }
