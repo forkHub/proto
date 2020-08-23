795e923fbb3ac28197866befd494813a84635675
fajar <rokhman.fajar@gmail.com>
2020-08-23 20:44:50 +0700

    a

--- blog/responsif/js/js.js
+++ blog/responsif/js/js.js
@@ -1,8 +1,6 @@
 "use strict";
 let canvas;
 let canvasCtx;
-let canvasScaleX = 1;
-let canvasScaleY = 1;
 let gambar;
 window.onload = () => {
     canvas = document.body.querySelector('canvas');
@@ -13,8 +11,8 @@ window.onload = () => {
     resize();
 };
 function resize() {
-    let cp = parseInt(canvas.style.width);
-    let cl = parseInt(canvas.style.height);
+    let cp = 360;
+    let cl = 218;
     let wp = window.innerWidth;
     let wl = window.innerHeight;
     let ratio = Math.min((wp / cp), (wl / cl));
@@ -24,10 +22,6 @@ function resize() {
     canvas.style.height = cl2 + 'px';
     canvas.style.top = ((wl - cl2) / 2) + 'px';
     canvas.style.left = ((wp - cp2) / 2) + 'px';
-    canvas.width = 360;
-    canvas.height = 218;
-    canvasScaleX = 360 / cp2;
-    canvasScaleY = 218 / cl2;
     gambarCanvas(canvasCtx);
 }
 function gambarCanvas(ctx) {
