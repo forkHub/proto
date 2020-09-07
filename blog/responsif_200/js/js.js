a23f70d5d5108a6246001d17bb174e620c791b41
fajar <rokhman.fajar@gmail.com>
2020-09-07 22:33:51 +0700

    'a'

---
+++ blog/responsif_200/js/js.js
@@ -0,0 +1,32 @@
+"use strict";
+let canvas;
+let canvasCtx;
+let gambar;
+window.onload = () => {
+    canvas = document.body.querySelector('canvas');
+    canvasCtx = canvas.getContext("2d");
+    gambar = document.body.querySelector('img.gambar');
+    gambarCanvas(canvasCtx);
+    window.onresize = resize;
+    resize();
+    setTimeout(() => {
+        resize();
+    }, 100);
+};
+function resize() {
+    let cp = 360;
+    let cl = 218;
+    let wp = window.innerWidth;
+    let wl = window.innerHeight;
+    let ratio = Math.max((wp / cp), (wl / cl));
+    let cp2 = Math.floor(cp * ratio);
+    let cl2 = Math.floor(cl * ratio);
+    canvas.style.width = cp2 + 'px';
+    canvas.style.height = cl2 + 'px';
+    canvas.style.top = ((wl - cl2) / 2) + 'px';
+    canvas.style.left = ((wp - cp2) / 2) + 'px';
+    gambarCanvas(canvasCtx);
+}
+function gambarCanvas(ctx) {
+    ctx.drawImage(gambar, 0, 0);
+}
