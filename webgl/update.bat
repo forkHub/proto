7157593d82c01be7de817ba9e240ddca9612256c
fajar <rokhman.fajar@gmail.com>
2020-09-27 03:17:03 +0700

    a

--- webgl/update.bat
+++ webgl/update.bat
@@ -7,6 +7,7 @@ del .\gbr\*.* /s /q
 del .\demo1\*.* /s /q
 del .\demo2\*.* /s /q
 del .\demo3\*.* /s /q
+del .\demo4\*.* /s /q
 
 rd .\css\ /s /q
 rd .\js\ /s /q
@@ -17,5 +18,6 @@ rd .\gbr /s /q
 xcopy "D:\xampp3\htdocs\proto\webgl\demo01\web\*.*" .\demo1 /s /y
 xcopy "D:\xampp3\htdocs\proto\webgl\demo02_skala\web\*.*" .\demo02 /s /y
 xcopy "D:\xampp3\htdocs\proto\webgl\demo03_rotasi\web\*.*" .\demo03 /s /y
+xcopy "D:\xampp3\htdocs\proto\webgl\demo04_anim\web\*.*" .\demo04 /s /y
 
 pause
