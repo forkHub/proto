f4f56044885c752ce83d13c32db5dc58dc97dcbb
fajar <rokhman.fajar@gmail.com>
2021-02-03 06:33:51 +0700

    a

---
+++ moon/zip.bat
@@ -0,0 +1,21 @@
+del \drive\moon\100_path-finding.zip
+del \drive\moon\200_click_01.zip
+del \drive\moon\250_click_01_scale.zip
+del \drive\moon\300_jalan_simple.zip
+del \drive\moon\400_jalan.zip
+del \drive\moon\500_astar.zip
+del \drive\moon\550_refaktor.zip
+del \drive\moon\600_config.zip
+del \drive\moon\900_jarak.zip
+
+7z a -tzip \drive\moon\file.zip .\100_path-finding -r
+7z a -tzip \drive\moon\100_path-finding .\100_path-finding -r
+7z a -tzip \drive\moon\200_click_01 .\200_click_01 -r
+7z a -tzip \drive\moon\250_click_01_scale .\250_click_01_scale -r
+7z a -tzip \drive\moon\300_jalan_simple .\300_jalan_simple -r
+7z a -tzip \drive\moon\400_jalan .\400_jalan -r
+7z a -tzip \drive\moon\500_astar .\500_astar -r
+7z a -tzip \drive\moon\550_refaktor .\550_refaktor -r
+7z a -tzip \drive\moon\600_config .\600_config -r
+7z a -tzip \drive\moon\900_jarak .\900_jarak -r
+pause
