163c53abb8fd5c4bee51398e014b3937ae1bce22
fajar <rokhman.fajar@gmail.com>
2021-02-19 15:09:51 +0700

    a

---
+++ gimbot/js/Gui.js
@@ -0,0 +1,29 @@
+"use strict";
+let GUI = {};
+GUI.kanan = document.body.querySelector('button.kanan');
+GUI.kiri = document.body.querySelector('button.kiri');
+GUI.aksi = document.body.querySelector('button.aksi');
+GUI.kanan.ontouchstart = (e) => {
+    e.stopPropagation();
+    console.log('kanan di pencet');
+};
+GUI.kanan.ontouchend = (e) => {
+    e.stopPropagation();
+    console.log('kanan di lepas');
+};
+GUI.kiri.ontouchstart = (e) => {
+    e.stopPropagation();
+    console.log('kiri di pencet');
+};
+GUI.kiri.ontouchend = (e) => {
+    e.stopPropagation();
+    console.log('kiri di lepas');
+};
+GUI.aksi.ontouchstart = (e) => {
+    e.stopPropagation();
+    console.log('tombol aksi di pencet');
+};
+GUI.aksi.ontouchend = (e) => {
+    e.stopPropagation();
+    console.log('tombol aksi di lepas');
+};
