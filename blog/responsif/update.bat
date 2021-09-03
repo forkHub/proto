a24f8b9d2e00be434d8f4ff593b723288dcb59dd
fajar <rokhman.fajar@gmail.com>
2021-09-03 09:50:04 +0700

    a

--- blog/responsif/update.bat
+++ blog/responsif/update.bat
@@ -8,5 +8,5 @@ rd .\js\ /s /q
 rd .\template /s /q
 
 rem xcopy D:\xampp3\htdocs\proto\blog\responsif\web .\ /s /y /i
-xcopy F:\soft\xampp7\htdocs\proto\blog\resp\responsif\web .\ /s /y /i
+xcopy ..\..\..\..\proto\blog\resp\responsif\web .\ /s /y /i
 pause
